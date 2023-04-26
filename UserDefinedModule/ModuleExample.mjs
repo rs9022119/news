354a5dbb90f80cbe2af54691f1fc2cd52b338d7f
unknown <rs9022119@gmail.com>
2023-04-27 02:13:27 +0530

    add

---
+++ UserDefinedModule/ModuleExample.mjs
@@ -0,0 +1,14 @@
+var name = "Rahul Kumar"
+var a = [10,20,30,40,50,60,70,80,90]
+var emp = {
+    id:1001,
+    name:"Rahul Kumar",
+    dsg:"Engineer",
+    salary:"25000",
+    city:"New Delhi"
+}
+function display(){
+    console.log("In Display Function");
+}
+export default display
+export {name,a,emp}
---
+++ UserDefinedModule/sample.js
@@ -0,0 +1,13 @@
+var name = "Rahul Kumar"
+var a = [10,20,30,40,50,60,70,80,90]
+var emp = {
+    id:1001,
+    name:"Rahul Kumar",
+    dsg:"Engineer",
+    salary:"25000",
+    city:"New Delhi"
+}
+function display(){
+    console.log("In Display Function");
+}
+module.exports = [name,a,emp,display]
